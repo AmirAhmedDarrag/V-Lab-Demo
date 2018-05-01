1f5c2105197846b4da3eb5054f9926b31143e84e
Amir <amirdarrag2013@gmail.com>
2018-05-01 13:40:52 +0200

    Add Rotate and Scale Action

--- Assets/Scripts/OrgansManager.cs
+++ Assets/Scripts/OrgansManager.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class OrgansManager : MonoBehaviour
 {
-
+	public bool OrgansTrigger;
 	public static int _OrgansIndex;
 	public static int _moreText;
 	public static int _soundIndex;
+	private Quaternion Org_Rotation;
 	public GameObject[] _Organs;
+	Vector3 new_scale, org_scale;
+	float value;
+	private int _RotationNum;
+	public GameObject _Temp, _FPC, _Camera;
 
 	void Awake ()
 	{
-
+		OrgansTrigger = false;
 		//initialize 
 		_OrgansIndex = -1;
 		_moreText = 0;
+		_RotationNum = 0;
 	}
 
+	void Update ()
+	{
+		if (OrgansTrigger) {
+			if (Input.GetKeyDown (KeyCode.JoystickButton1)) {
+				value = 0.1f;
+				if ((new_scale.x < org_scale.x + 1.0f))
+					scale (value);
+			} else if (Input.GetKeyDown (KeyCode.JoystickButton3)) {
+				value = -0.1f;
+				if ((new_scale.x > org_scale.x + 0.2f))
+					scale (value);
+			}
+			if (Input.GetKeyDown (KeyCode.JoystickButton7)) {
+				_RotationNum++;
+				if (_RotationNum % 2 != 0) {
+					_Camera.transform.parent = _Temp.transform;
+					_FPC.SetActive (false);
+				} else {
+					_Camera.transform.parent = _FPC.transform;
+					_FPC.SetActive (true);
+				}
+			}
+
+			if (_RotationNum % 2 != 0) {
+				float horizontalMove = Input.GetAxisRaw ("Horizontal");
+				float vertiaclMove = Input.GetAxisRaw ("Vertical");
+
+				if (horizontalMove != 0 && vertiaclMove != 0) {
+					//Debug.Log ("kk");
+					_Organs [_OrgansIndex].transform.Rotate (horizontalMove, 0, vertiaclMove);
+				} else {
+					//Debug.Log ("ssss");
+					_Organs [_OrgansIndex].transform.Rotate (vertiaclMove, horizontalMove, 0);
+				}
+			}
+		}
+	}
 	//Organs Trigger(Action)
 	public void OnOrgansTrigger (int index)
 	{
+		OrgansTrigger = true;
 		//set All Organs by false except the Trigger Organs
 		for (int i = 0; i < _Organs.Length; i++) {
 			if (i == index) {
@@ -29,9 +73,10 @@ public class OrgansManager : MonoBehaviour
 			} else {
 				_Organs [i].SetActive (false);
 			}
-			_OrgansIndex = index;
 		}
-			
+		_OrgansIndex = index;
+		org_scale = _Organs [_OrgansIndex].transform.localScale;
+		Org_Rotation = _Organs [_OrgansIndex].transform.rotation;	
 	}
 
 	public void OnOrganstouch (int MoreText)
@@ -54,7 +99,8 @@ public class OrgansManager : MonoBehaviour
 			
 			_Organs [i].SetActive (true);
 		}
-
+		_Organs [_OrgansIndex].transform.localScale = org_scale;
+		_Organs [_OrgansIndex].transform.rotation = Org_Rotation;
 		// set shared Index by -1
 		_OrgansIndex = -1;
 
@@ -67,6 +113,19 @@ public class OrgansManager : MonoBehaviour
 
 		//reset Sound
 		TextManager.NPSound = false;
+
+		OrgansTrigger = false;
+
+        
+	}
+
+	public void scale (float value)
+	{
+
+		new_scale.x = _Organs [_OrgansIndex].transform.localScale.x + value;
+		new_scale.y = _Organs [_OrgansIndex].transform.localScale.y + value;
+		new_scale.z = _Organs [_OrgansIndex].transform.localScale.z + value;
+		_Organs [_OrgansIndex].transform.localScale = new_scale;
 
 	}
 }
